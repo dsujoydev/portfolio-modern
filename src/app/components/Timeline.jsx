da4aaee10cd1cfda507bd1a8c2112aef9745ca01
dsujoydev <dsujoy.cse@gmail.com>
2023-06-28 16:21:02 +0600

    timeline

---
+++ src/app/components/Timeline.jsx
@@ -0,0 +1,49 @@
+"use client";
+import React from "react";
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { MdWork } from "react-icons/md";
+
+export const Timeline = () => {
+  return (
+    <div>
+      <VerticalTimeline>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{
+            background: "linear-gradient(to left bottom, #dce7e3, #d9e9dd, #dde9d3, #e6e7c9, #f4e4c0)",
+            color: "black",
+            borderRadius: "10px",
+          }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          date="April 2023 - present"
+          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          icon={<MdWork />}
+        >
+          <h3 className="vertical-timeline-element-title">Front-End Developer</h3>
+          <h4 className="vertical-timeline-element-subtitle">Ragnets Software LTD.</h4>
+          <p>Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+      <VerticalTimeline>
+        <VerticalTimelineElement
+          position="right"
+          className="vertical-timeline-element--work"
+          contentStyle={{
+            background: "linear-gradient(to left bottom, #dce7e3, #d9e9dd, #dde9d3, #e6e7c9, #f4e4c0)",
+            color: "black",
+            borderRadius: "10px",
+          }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          date="February 2023 - April 2023"
+          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          icon={<MdWork />}
+        >
+          <h3 className="vertical-timeline-element-title"> Intern Front-End Developer</h3>
+          <h4 className="vertical-timeline-element-subtitle">Ragnets Software LTD.</h4>
+          <p>Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
+};
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
-// import Timeline from "./components/Timeline";
+import { Timeline } from "./components/Timeline";
 
 export default function Home() {
   return (
@@ -18,7 +18,9 @@ export default function Home() {
         <Projects />
       </section>
 
-      <section>{/* <Timeline /> */}</section>
+      <section>
+        <Timeline />
+      </section>
     </main>
   );
 }
