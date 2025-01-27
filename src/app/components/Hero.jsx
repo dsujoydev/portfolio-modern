33154d110bb1efdb7bd9385595c6d06ce024dff0
dsujoydev <dsujoy.cse@gmail.com>
2025-01-27 23:00:09 +0600

    feat: update Hero component description for clarity and accuracy

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -72,9 +72,10 @@ export default function Hero() {
       <div className="h-[30rem] sm:h-[40rem] md:h-[30rem] rounded-3xl p-10 flex flex-col gap-4 sm:gap-6 md:gap-8 bg-hero bg-cover">
         <h1 className="text-2xl sm:text-3xl md:text-4xl font-semibold">HI, I am Sujoy Das, A Front-End Developer</h1>
         <p className={`${arima.className} flex-1 text-sm sm:text-base md:text-lg text-justify leading-relaxed`}>
-          Passionate about Web development with advance skills in HTML, CSS, JS. I have knowledge of popular frameworks
-          such as ReactJs, NextJs as well. I have a reliable analytical & debugging skill. Active with research &
-          development, new frameworks, new stacks as curious about learning new technologies.
+          Passionate about Full Stack Web development with advance skills in TS, JS. I have advanced knowledge of
+          ReactJs, with popular frameworks such as , NextJs, Astro. I have a reliable analytical & debugging skill.
+          Active with research & development, exploring new frameworks, new stacks as curious about learning new
+          technologies.
         </p>
         <div className="flex flex-col sm:flex-row items-center gap-4 sm:gap-12 justify-center sm:justify-self-end">
           <button className="bg-black text-white font-medium py-2 sm:py-3 px-6 sm:px-12 rounded-full w-full sm:w-60 lg:w-auto">
