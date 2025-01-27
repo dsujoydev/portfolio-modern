be81d1fc58be0aaeb402c0c101ae169a368f53e7
dsujoydev <dsujoy.cse@gmail.com>
2025-01-27 22:58:07 +0600

    feat: add new projects with updated descriptions and images

--- src/app/page.js
+++ src/app/page.js
@@ -12,11 +12,36 @@ export default function Home() {
       <section id="projects" className="grid grid-cols-1 lg:grid-cols-2 gap-4 my-4 mb-6">
         <Projects
           name="A interactive e-commerce store with fake store API"
-          description="React, Tailwind"
+          description="Javascript React, Tailwind"
           imgUrl="bg-mockuptwo"
           bgColor="#9db1bc"
           url="https://fakestore-cart-das.netlify.app/"
         />
+
+        <Projects
+          name="A job hunt website"
+          description="Typescript, React, Tailwind"
+          imgUrl="bg-mockupfour"
+          url="https://careerhubph.netlify.app/"
+          bgColor="#9db1bc"
+        />
+
+        <Projects
+          name="A full-stack learning management system"
+          description="Typescript, React, Tailwind, Shadcn, Django"
+          imgUrl="bg-mockupfive"
+          bgColor="#9db1bc"
+          url="https://edge-lms-django-react-1.onrender.com/"
+        />
+
+        <Projects
+          name="Uncode - A documentation site with Docusaurus"
+          description="Typescript, MDX, Docusaurus"
+          url="https://uncode-doc.vercel.app/"
+          imgUrl="bg-mockupsix"
+          bgColor="#9db1bc"
+        />
+
         <Projects
           name="A architect farm Website"
           description="Made with HTML, CSS"
@@ -29,10 +54,6 @@ export default function Home() {
           imgUrl="bg-mockupthree"
           bgColor="#9db1bc"
         />
-
-        <Projects bgColor="#9db1bc" />
-        <Projects bgColor="#9db1bc" />
-        <Projects bgColor="#9db1bc" />
       </section>
 
       <section id="experiene">
